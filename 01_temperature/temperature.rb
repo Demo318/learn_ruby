78ef8a29b7673b5a532fc0b6097abd93bc7076e2
Devin Mork <devin.mork@gmail.com>
2017-04-04 18:45:55 -0500

    Complete 00, 01, 02. Start 03.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,14 @@
 #write your code here
+def ftoc temp_f
+
+temp_c = ((((temp_f - 32)* 1.0) * 5.0) / 9.0)
+  return temp_c
+end
+
+def ctof temp_c
+
+temp_f = ((((temp_c * 1.0) * 9.0) / 5.0) +32)
+if temp_f % 1 == 0
+  temp_f = temp_f.to_i
+  return temp_f
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
 #write your code here
+def echo string
+  return string
+end
+
+def shout string
+  return string.upcase
+end
+
+def repeat string, int = 0
+  if int <= 1
+    return string + " " + string
+  else
+    rpt = int - 1
+    add = ""
+    rpt.times do
+      add = add + " " + string
+    end
+    return string + add
+  end
+end
+
+def start_of_word entry, int = 1
+  start = Array.new entry.split("")
+  result = Array.new
+  start.each do |x|
+    if x < int
+      result.push(start[x])
+    end
+  end
+  puts entry
+  puts start
+  puts result
+  return start
+end
+
+start_of_word "Bentley"

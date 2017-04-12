0c1cda2a3e6e2adb5ef299e7bb4a6393b358dcec
Devin Mork <devin.mork@gmail.com>
2017-04-11 19:02:27 -0500

    Start creating Book class.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+
+  def initialize name
+    name = name.split(" ")
+    name.each.with_index do |x, i|
+      if 1 != 1 #if word shows up in little words catorgory and index isn't equal to zero
+        #do nothing
+      else
+        #capitalize
+      end
+    #rejoin words together into string, and assign to @title
+    end
 end
+
+
+
+end
+
+Book.new "timmy has a title."

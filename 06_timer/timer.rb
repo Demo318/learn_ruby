f18a30c655bab3390f7f953d5299f22cef284cfe
Devin Mork <devin.mork@gmail.com>
2017-04-11 21:11:38 -0500

    Organize logic for breaking down seconds into hours/minutes/seconds.

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+
+
+
 end
+puts "How many seconds?"
+number = gets.chomp.to_i
+hours = number / (60*60)
+number = number % (60*60)
+minutes = number / 60
+number = number % 60
+seconds = number
+number = number - seconds
+
+puts "Hours: #{hours}."
+puts "Minutes: #{minutes}."
+puts "Seconds: #{seconds}."
+puts "There are #{number}."
